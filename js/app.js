7e83007edb03f13cc1069d3e874c753b3b673833
Matthew Woodley <Woodlehh@gmail.com>
2019-02-21 15:49:05 +0000

    end of lab

--- js/app.js
+++ js/app.js
@@ -1,3 +1,47 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
+
+  const handleTitle = (event) => {
+    console.dir(event);
+    console.log('Title typed');
+    const title = event.target.value;
+    console.log(event.target.value);
+    const resultParagraph = document.querySelector('#title-result');
+    resultParagraph.textContent = `${title}`
+  }
+
+  const titleInput = document.querySelector('#title');
+  titleInput.addEventListener('input', handleTitle);
+
+  const handleAuthor = (event) => {
+    console.dir(event);
+    console.log('Author typed');
+    const author = event.target.value;
+    console.log(event.target.value);
+    const resultParagraph = document.querySelector('#author-result');
+    // resultParagraph.textContent = `${author}`
+  }
+
+  const authorInput = document.querySelector('#author');
+  authorInput.addEventListener('input', handleAuthor);
+
+  const handleSelectChange = (event) => {
+    console.log('Something has been selected!');
+    const select = event.target.value;
+    const resultParagraph = document.querySelector('#select-result');
+    // resultParagraph.textContent = `${event.target.value}`;
+  }
+
+  const select = document.querySelector('#category');
+  select.addEventListener('change', handleSelectChange);
+
+  const form = document.querySelector('#form');
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const title = event.target.title.value;
+    const author = event.target.author.value;
+    const category = event.target.category.value;
+    const resultParagraph = document.querySelector('#form-result');
+  })
+
 })
